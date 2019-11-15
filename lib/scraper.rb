a43441831a3ee69f8228c8c0958e1367f3d73099
Wendy Harris <wendyv510@hotmail.com>
2019-11-15 22:30:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,7 +11,7 @@ class Scraper
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end 
   
-  def get_courses 
+  def get_courses(css)
     doc.css(".post").first.css("h2").text
   end 
   
